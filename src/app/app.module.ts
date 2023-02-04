967f1e383d17cf6943f7f233ba402b7ffc86702d
KarlMercado <83860193+KarlMercado@users.noreply.github.com>
2023-02-04 19:05:10 +0800

    2 files modified, 12 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,34 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+
+import { Routes, RouterModule } from '@angular/router';
+
 import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { ReferencesComponent } from './references/references.component';
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'achievements', component: AchievementsComponent },
+  { path: 'References', component: ReferencesComponent },
+];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, AboutComponent],
+  imports: [RouterModule.forRoot(routes), BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    AboutComponent,
+    SkillsComponent,
+    EducationComponent,
+    AchievementsComponent,
+    ReferencesComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
